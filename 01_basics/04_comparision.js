13ebcd84d398226235df456ecbfc1ced47c858a2
Hitesh-Chordiya <55180911+Hitesh-Chordiya@users.noreply.github.com>
2023-11-05 08:14:00 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -11,11 +11,11 @@
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
+//equality operator didn't convert null to 0 but the comparison operator did convert it to 0, that's why >=gave true
 
 console.log(undefined == 0);
 console.log(undefined > 0);
-console.log(undefined < 0);
-
-// === 
+console.log(undefined < 0);  
+// === strict check, checks type and value too
 
 console.log("2" === 2);
